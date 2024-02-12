9d047ebf331b4661619d24d32f12dff4f66e7f94
Nguyễn Mạnh Vũ <31726319+manhvu0803@users.noreply.github.com>
2024-02-12 15:41:10 +0700

    Add scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,12 @@ class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData(
+        useMaterial3: true,
+        textTheme: Theme.of(context).textTheme.apply(
+          fontSizeFactor: 1.1
+        )
+      ),
       home: const MainScreen()
     );
   }
@@ -28,14 +33,37 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (index) => setState(() => selectedPageIndex = index),
-        selectedIndex: selectedPageIndex,
-        destinations: const [
-          NavigationDestination(icon: Icon(Icons.home), label: "Home"),
-          NavigationDestination(icon: Icon(Icons.search), label: "Search"),
-        ],
+    return SafeArea(
+      child: DefaultTabController(
+        initialIndex: 1,
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const TabBar(
+              indicatorSize: TabBarIndicatorSize.tab,
+              tabs: [
+                Tab(text: "All"),
+                Tab(text: "Today"),
+                Tab(text: "Upcoming")
+              ]
+            ),
+          ),
+          bottomNavigationBar: NavigationBar(
+            onDestinationSelected: (index) => setState(() => selectedPageIndex = index),
+            selectedIndex: selectedPageIndex,
+            destinations: const [
+              NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+              NavigationDestination(icon: Icon(Icons.search), label: "Search"),
+            ],
+          ),
+          floatingActionButton: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: FloatingActionButton(
+              onPressed: () {},
+              child: const Icon(Icons.add),
+            ),
+          ),
+        ),
       ),
     );
   }
